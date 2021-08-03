34b1d57402dfa425cf846bb436d6f405d6d3884c
Big Coight <ajzulli@gmail.com>
2021-08-04 09:54:16 +1000

    Week 2 content added

---
+++ Week2/main.js
@@ -0,0 +1,54 @@
+/*
+Surname     | Firstname        | email    | Contribution% | Any issues?
+=======================================================================
+Zulli       |Anthony Nicholas  | azul0005 | 25%           |
+Morgante    |Giacomo           | gmor0008 | 25%           |
+Lai         |Glorison          | glai0004 | 25%           |
+Ho          |Joshua            | jhoo0017 | 25%           |
+
+Complete Worksheet 2 by entering code in the places marked below...
+
+For full instructions and tests open the file worksheetChecklist.html
+in Chrome browser.  Keep it open side-by-side with your editor window.
+You will edit this file (main.js), save it, and reload the
+browser window to run the test.
+*/
+/**
+ * Exercise 1
+ */
+
+/**
+ * Exercise 2
+ */
+
+/**
+ * Exercise 3
+ */
+
+/**
+ * Exercise 4
+ */
+
+/**
+ * Exercise 5
+ */
+
+/**
+ * Exercise 6
+ */
+
+/**
+ * Exercise 7
+ */
+
+/**
+ * Exercise 8
+ */
+
+/**
+ * Exercise 9
+ */
+
+/**
+ * Exercise 10
+ */
